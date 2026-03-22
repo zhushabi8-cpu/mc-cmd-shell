8c8d9e760bc88f9251b6baae31321cd8ee3ee1a1
PPPfanya <zhushabi8@gmail.com>
2026-03-22 12:31:03 +0800

    Add files via upload

---
+++ src/main/java/com/ppp/ModMain.java
@@ -0,0 +1,28 @@
+package com.ppp;
+
+import com.ppp.command.ModCommands;
+import com.ppp.util.AdminUtil;
+import com.ppp.util.RegistryBackup;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class ModMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            if (!AdminUtil.isAdmin()) {
+                AdminUtil.elevate();
+            }
+        }
+
+        ModCommands.register();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (com.ppp.util.OSDetector.isWindows() &&
+                    FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+                RegistryBackup.restore();
+            }
+        }));
+    }
+}
